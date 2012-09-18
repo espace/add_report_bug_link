3f8e37b2eb85beb7d83db7455be34d72ec628f2e
Hager Abdelmotaal <hager.abdelmotaal@espace.com.eg>
2012-09-18 19:23:49 +0200

    change  patching mechanism  in Rails 3, – one should use Dispatcher functionality (Rails 2.x) and another – new to_prepare functionality from Rails 3.

--- init.rb
+++ init.rb
@@ -1,20 +1,30 @@
 # Include hook code here
 require 'redmine'
-require 'dispatcher'
+require 'dispatcher'  unless Rails::VERSION::MAJOR >= 3
+
 # Hooks
 require_dependency 'my_hooks'
 
-Dispatcher.to_prepare do
-  require_dependency 'issue'
-  require 'issue_patch'
-  Issue.send( :include, IssuePatch)
+
+if Rails::VERSION::MAJOR >= 3
+   ActionDispatch::Callbacks.to_prepare do
+    require_dependency 'issue'
+    require 'issue_patch'
+    Issue.send( :include, IssuePatch)
+  end
+else
+  Dispatcher.to_prepare do
+    require_dependency 'issue'
+    require 'issue_patch'
+    Issue.send( :include, IssuePatch)
+  end
 end
 
 Redmine::Plugin.register :add_report_bug_link do
   name 'Add Report Bug Link'
   author 'Basayel Said'
   description 'As a team member, can click on a "Open an Issue on this Story" link in the side bar of any story.'
-  version '0.0.1' 
+  version '0.0.1'
 end
 
 Redmine::AccessControl.map do |map|
@@ -24,6 +34,7 @@ Redmine::AccessControl.map do |map|
 end
 
 #fix required to make the plugin work in devel mode with rails 2.2
-load_paths.each do |path|
-  ActiveSupport::Dependencies.load_once_paths.delete(path)
+Rails.configuration.autoload_paths.each do |path|
+  ActiveSupport::Dependencies.autoload_once_paths.delete(path)
 end
+
