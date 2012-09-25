e94277cedc65853f9a0c87f72f550915e9a04917
Yasmine Gaber <yasmine.gaber@espace.com.eg>
2012-09-25 12:02:46 +0200

    remove Report A Bug permission

--- init.rb
+++ init.rb
@@ -30,15 +30,8 @@ Redmine::Plugin.register :add_report_bug_link do
   version '0.0.1'
 end
 
-Redmine::AccessControl.map do |map|
-  map.project_module :issue_tracking do |map|
-    map.permission :report_bug, {:my_issues =>:my_new}
-  end
-end
-
 #fix required to make the plugin work in devel mode with rails 2.2
 Rails.configuration.autoload_paths.each do |path|
-  puts ">>>>>>>>>>>>>>" + path
-   ActiveSupport::Dependencies.autoload_once_paths.delete(path)
+  ActiveSupport::Dependencies.autoload_once_paths.delete(path)
 end
 
